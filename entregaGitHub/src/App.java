0fff40fcbf14fa5b2d9f1edb28234add896a0302
mario <mariohernandez2112003@gmail.com>
2026-05-07 19:22:29 +0200

    He añadido ejericio 7

--- entregaGitHub/src/App.java
+++ entregaGitHub/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Este era el repositorio de hector ");
+        System.out.println("Ejercicio 7 ");
     }
 }
