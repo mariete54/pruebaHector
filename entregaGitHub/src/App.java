66bc612588186d35bf2a4a6d634b32d4a35f1e01
mario <mariohernandez2112003@gmail.com>
2026-05-07 19:29:40 +0200

    He añadido ejercicio8b

--- entregaGitHub/src/App.java
+++ entregaGitHub/src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Este era el repositorio de hector ");
         System.out.println("Ejercicio 7 ");
+          System.out.println("Ejercicio 8b");
+    
     }
 }
